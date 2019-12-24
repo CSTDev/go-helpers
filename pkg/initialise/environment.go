b6a622a7518e7350fbba25032e0101a61bff8c25
Carl Taylor <carl.taylor@triad.co.uk>
2019-12-24 09:39:34 +0000

    Add method to read integer environment variables

--- pkg/initialise/environment.go
+++ pkg/initialise/environment.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	log "github.com/sirupsen/logrus"
 	"os"
+	"strconv"
 )
 
 // GetEnvironmentVariable will try to find the value for the provided key
@@ -26,4 +27,32 @@ func OptionalEnvironmentVariable(key string) (string, error) {
 		return "", errors.New("Missing environment variable: " + key)
 	}
 	return variable, nil
+}
+
+// GetEnvironmentVariableInt will try to find the value for the provided key
+// If it can't be found it logs and exits the program
+func GetEnvironmentVariableInt(key string) int {
+	variable := os.Getenv(key)
+	if variable == "" {
+		log.Error("Missing environment variable: " + key)
+		os.Exit(1)
+	}
+
+	value, err := strconv.Atoi(variable)
+	if err != nil {
+		log.Error("error converting value to string")
+		os.Exit(1)
+	}
+	return value
+}
+
+// OptionalEnvironmentVariableInt will try to find the value for the provided key
+// If it can't be found it logs and returns and error that can be ignored
+func OptionalEnvironmentVariableInt(key string) (int, error) {
+	variable := os.Getenv(key)
+	if variable == "" {
+		log.Warn("Missing environment variable: " + key)
+		return -1, errors.New("Missing environment variable: " + key)
+	}
+	return strconv.Atoi(variable)
 }
--- pkg/initialise/environment_test.go
+++ pkg/initialise/environment_test.go
@@ -1,10 +1,11 @@
-package initialise
+package initialise_test
 
 import (
 	"os"
 	"os/exec"
 	"testing"
 
+	"github.com/cstdev/go-helpers/pkg/initialise"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -16,7 +17,7 @@ func TestMain(m *testing.M) {
 
 func TestGetEnvironmentVariableKillsWhenNotFound(t *testing.T) {
 	if os.Getenv("NO_ENV") == "1" {
-		GetEnvironmentVariable("DOES_NOT_EXIST")
+		initialise.GetEnvironmentVariable("DOES_NOT_EXIST")
 		return
 	}
 	cmd := exec.Command(os.Args[0], "-test.run=TestGetEnvironmentVariableKillsWhenNotFound")
@@ -31,7 +32,7 @@ func TestGetEnvironmentVariableKillsWhenNotFound(t *testing.T) {
 func TestGetEnvironmentVariableReturnsValue(t *testing.T) {
 	testValue := "OK"
 	os.Setenv("TEST_VAL", testValue)
-	res := GetEnvironmentVariable("TEST_VAL")
+	res := initialise.GetEnvironmentVariable("TEST_VAL")
 	if res != testValue {
 		t.Fatalf("Result: %s \n Actual %s", res, testValue)
 	}
@@ -39,8 +40,24 @@ func TestGetEnvironmentVariableReturnsValue(t *testing.T) {
 
 func TestOptionalEnvironmentVariableReturnsError(t *testing.T) {
 	os.Clearenv()
-	_, err := OptionalEnvironmentVariable("TEST_VAL")
+	_, err := initialise.OptionalEnvironmentVariable("TEST_VAL")
 	if err == nil {
 		t.Fatal("expected error did not return")
 	}
+}
+
+func TestGetEnvironmentVariableIntKillsWhenString(t *testing.T) {
+	if os.Getenv("NO_ENV") == "1" {
+		os.Setenv("TEST_VAL", "STRING")
+		_ = initialise.GetEnvironmentVariableInt("TEST_VAL")
+	}
+
+	cmd := exec.Command(os.Args[0], "-test.run=TestGetEnvironmentVariableIntKillsWhenString")
+	cmd.Env = append(os.Environ(), "NO_ENV=1")
+	err := cmd.Run()
+	if e, ok := err.(*exec.ExitError); ok && !e.Success() {
+		return
+	}
+	t.Fatalf("process ran with error %v, want exist status 1", err)
+
 }
